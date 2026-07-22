ca9e8441d56c1b683d18a71e0a9e2fb89bced803
ayushcoders-master <ayushkrgupta05@gmail.com>
2026-07-22 18:13:36 +0000

    string

---
+++ 01_basics/03_strings.js
@@ -0,0 +1,10 @@
+const name = "AYUSH"
+const repCount = 50
+
+console.log(`My name is ${name} and my repository count is ${repCount}`)
+
+console.log(name.charAt(2))
+
+console.log(name[0])
+
+console.log(name.__proto__)
