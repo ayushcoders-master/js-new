f77e4936cece714080ee9941d2aead13c6600c2c
ayushcoders-master <ayushkrgupta05@gmail.com>
2026-07-22 18:27:41 +0000

    strings_update

--- 01_basics/03_strings.js
+++ 01_basics/03_strings.js
@@ -10,3 +10,19 @@ console.log(name[0])
 console.log(name.__proto__)
 
 console.log(name.indexOf('Y'))
+
+console.log(name.substring(0,4))
+
+
+const newString = "   student  "
+
+console.log(newString)
+console.log(newString.trim())
+
+const url = "http://172.16.16.16:8090/httpclient.html"
+
+console.log(url.replace("client" ,"ayush"))
+
+console.log(url.includes("sundar")) // check wheather it is include in the url or not
+
+console.log(name.split('-'))
