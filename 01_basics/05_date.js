aab4b21134792eb3fdab8a9d036ffdfa4c40bbbf
ayushcoders-master <ayushkrgupta05@gmail.com>
2026-07-23 17:58:38 +0000

    basics

---
+++ 01_basics/05_date.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())  
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toISOString())
+
+
+console.log(myDate.toJSON())
+
+console.log(myDate.toTimeString())
+
+
+let myCreateDate = new Date(2026,6,23,23,6)
+console.log(myCreateDate.toLocaleString())
